a4dfd396105438aff4f1c26220f614b0219b153a
Matthew Balachowski <Mbalac3@lsu.edu>
2025-02-27 15:48:53 -0600

    added switching to two different scenes from the main page

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,88 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const MyApp());
 }
+class option1A extends StatelessWidget {
+  const option1A({super.key});
 
+  @override
+Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: const Text('This is option 1A')),
+      ),
+       backgroundColor: const Color.fromARGB(255, 69, 11, 136),
+       
+      body: Center(
+              child: Column(
+        mainAxisAlignment: MainAxisAlignment.center, // Centers content vertically
+        children: [
+         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+           Padding(
+             padding: const EdgeInsets.all(4.0),
+             child: Image.network('https://petlandleessummit.com/wp-content/uploads/2022/11/2502728_800-2-600x450.jpg', width:300, height:300),
+           ),
+                   
+        
+            
+          ],
+          
+        ),
+         Padding(
+           padding: const EdgeInsets.all(96.0),
+           child: const Text('This is not my dog, its a random stock image off Google that looks like my dog.  Its a shih-poo and I love that dawg. This reminds me of him',style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),),
+         ),
+            ]
+      ),
+      
+    )
+    );
+  }
+}
+class option1B extends StatelessWidget {
+  const option1B({super.key});
+
+  @override
+Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: const Text('This is option 1B')),
+      ),
+       backgroundColor: const Color.fromARGB(255, 69, 11, 136),
+       
+      body: Center(
+              child: Column(
+        mainAxisAlignment: MainAxisAlignment.center, // Centers content vertically
+        children: [
+         Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+           Padding(
+             padding: const EdgeInsets.all(4.0),
+             child: Image.network('https://petlandleessummit.com/wp-content/uploads/2022/11/2502728_800-2-600x450.jpg', width:300, height:300),
+           ),
+                   
+        
+            
+          ],
+          
+        ),
+         Padding(
+           padding: const EdgeInsets.all(96.0),
+           child: const Text('This is not my dog, its a random stock image off Google that looks like my dog.  Its a shih-poo and I love that dawg. This reminds me of him',style: TextStyle(color: Color.fromARGB(255, 255, 255, 255)),),
+         ),
+            ]
+      ),
+      
+    )
+    );
+  }
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+
 
   // This widget is the root of your application.
   @override
@@ -67,42 +146,17 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
-
+  
+//the below code will switch the button to the option1A 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
@@ -115,11 +169,33 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: 
+      Row(
+  mainAxisAlignment: MainAxisAlignment.end,
+  children: [
+    FloatingActionButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => option1A()),
+        );
+      },
+    
+      child: const Icon(Icons.arrow_forward),
+    ),
+    const SizedBox(width: 10), // Spacing between buttons
+    FloatingActionButton(
+      onPressed: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => option1B()),
+        );
+      },
+      child: const Icon(Icons.arrow_back),
+    ),
+  ],
+),
     );
+    
   }
 }
