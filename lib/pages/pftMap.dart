c674ec8c56f7044de987058e2687b7a697981ead
Sam <SamuelBustamante808@gmail.com>
2025-03-07 17:46:49 -0600

    updated map points

--- lib/pages/pftMap.dart
+++ lib/pages/pftMap.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:scavenger_hunt/pages/basf.dart';
+import 'package:scavenger_hunt/pages/centerofEng.dart';
+import 'package:scavenger_hunt/pages/chairroom.dart';
 import 'package:scavenger_hunt/pages/commonArea.dart';
+import 'package:scavenger_hunt/pages/donorwall.dart';
 import 'package:scavenger_hunt/pages/panera.dart';
 import 'package:scavenger_hunt/pages/rightHallway.dart';
+import 'package:scavenger_hunt/pages/stairarea.dart';
 
 void main() {
   runApp(MaterialApp(home: MapPage()));
@@ -26,10 +30,12 @@ class MapPage extends StatelessWidget {
               Image.asset("assets/map.png"),
               _buildHotspot(context, Panera(), "Panera", 440, 225),
               _buildHotspot(context, basf(), "BASF Lab", 545, 550),
-              // _buildHotspot(context, "bigstair", "Atrium Stairs", 810, 255),
-              // _buildHotspot(context, "centerofeng1", "Engineering Center", 980, 290),
+              _buildHotspot(context, Stairarea(), "Atrium Stairs", 810, 255),
+              _buildHotspot(context, centerofEng(), "Engineering Center", 980, 290),
               _buildHotspot(context, commonArea(), "Commmons", 560, 250),
               _buildHotspot(context, RightHallway(), "Hallway", 500, 400),
+              _buildHotspot(context, donorwall(), "Donor Wall", 985, 500),        
+              _buildHotspot(context, chairroom(), "Chair Room", 300, 275)      
             ],
           ),
         ),
